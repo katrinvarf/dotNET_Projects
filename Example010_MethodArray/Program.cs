fbdbcf782ecd2097fbf05e3d768f807efd81e4be
katvarf <varfolomeeva_97@bk.ru>
2022-09-20 09:10:50 +0300

    add Example010 MethodArray project

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 11, 22, 31, 4, 15, 16, 4, 81 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
