80a314fd3389a5e88c896f4e948d65733b20d6ac
katvarf <varfolomeeva_97@bk.ru>
2022-09-20 09:10:17 +0300

    add Example009 IntroArray project

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int a, int b, int c)
+{
+    int result = a;
+    if (b > result) result = b;
+    if (c > result) result = c;
+    return result;
+}
+
+int[] array = { 15, 21, 39, 12, 232, 33, 13, 23, 331 };
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(max);
